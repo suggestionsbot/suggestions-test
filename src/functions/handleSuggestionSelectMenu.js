9e6734778b2f5f6d9a560ce815a4880ebd9e4589
Anthony Collier <hello@acollier.dev>
2022-04-17 13:34:56 -0400

    :art: Update interaction vs replying for less messages
    Also handle the deletion of a suggestion with options to cancel or confirm the action

--- src/functions/handleSuggestionSelectMenu.js
+++ src/functions/handleSuggestionSelectMenu.js
@@ -1,43 +1,45 @@
-const { Client, SelectMenuInteraction } = require('discord.js');
+const { Client, SelectMenuInteraction, CacheType } = require('discord.js');
+
+const { suggestionDeleteRow } = require('../components/suggestionActions');
 
 /**
  * This function handles the selection of various suggestion management action.
  * @param {Client} client - The client instance.
- * @param {SelectMenuInteraction} interaction - The interaction being handled.
+ * @param {SelectMenuInteraction<CacheType>} interaction - The interaction being handled.
  */
 module.exports = async (client, interaction) => {
   if (interaction.customId !== 'manage-suggestion-options') return;
 
   switch (interaction.values[0]) {
     case 'approve-option': {
-      await interaction.reply({
+      await interaction.update({
         content: 'You have approved this suggestion!',
+        components: [],
         ephemeral: true,
       });
 
       return;
     }
     case 'reject-option': {
-      await interaction.reply({
+      await interaction.update({
         content: 'You have rejected this suggestion!',
+        components: [],
         ephemeral: true,
       });
 
       return;
     }
     case 'delete-option': {
-      setTimeout(async () => {
-        await client.channels.cache.get(interaction.channelId).delete();
-      }, 5000);
-
-      await interaction.reply(
-        "The suggestion and it's thread will be deleted in **5 seconds**!",
-      );
+      await interaction.update({
+        content: 'Please confirm that you want to delete this suggestion.',
+        components: [suggestionDeleteRow],
+        ephemeral: true,
+      });
 
       return;
     }
     default: {
-      await interaction.reply({
+      await interaction.update({
         content: `A select menu interaction was received, but I don't know what to do with it!`,
         ephemeral: true,
       });
