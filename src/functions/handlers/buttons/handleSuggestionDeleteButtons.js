7c031c9bc322224805f5f4b5bd90697f80e20305
Anthony Collier <hello@acollier.dev>
2022-04-17 19:32:58 -0400

    :art: Use deleteSuggestion function

--- src/functions/handlers/buttons/handleSuggestionDeleteButtons.js
+++ src/functions/handlers/buttons/handleSuggestionDeleteButtons.js
@@ -1,5 +1,6 @@
 const { Client, ButtonInteraction, CacheType } = require('discord.js');
 
+const deleteSuggestion = require('../../suggestions/deleteSuggestion');
 /**
  * This function handles the actions of deleting a suggestion..
  * @param {Client} client - The client instance.
@@ -9,7 +10,7 @@ module.exports = async (client, interaction) => {
   switch (interaction.customId) {
     case 'confirm-delete': {
       setTimeout(async () => {
-        await client.channels.cache.get(interaction.channelId).delete();
+        await deleteSuggestion(client, interaction);
       }, 5000);
 
       await interaction.reply(
