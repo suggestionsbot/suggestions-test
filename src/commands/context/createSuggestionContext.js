db050f64c7897e44e31cdb7d62e79c04bb1c2f55
Anthony Collier <hello@acollier.dev>
2022-04-17 23:36:17 -0400

    :bug: Rename modal

--- src/commands/context/createSuggestionContext.js
+++ src/commands/context/createSuggestionContext.js
@@ -2,16 +2,16 @@ const { ContextMenuCommandBuilder } = require('@discordjs/builders');
 const { ApplicationCommandType } = require('discord-api-types/v9');
 const { showModal } = require('discord-modals');
 
-const { suggestionModal } = require('../../components/suggestionModals');
+const { suggestionCreateModal } = require('../../components/suggestionModals');
 
 module.exports = {
   data: new ContextMenuCommandBuilder()
     .setName('Create Suggestion')
     .setType(ApplicationCommandType.Message),
   async execute(client, interaction) {
-    suggestionModal.components[1].components[0].value =
+    suggestionCreateModal.components[1].components[0].value =
       interaction.options.resolved.messages.first().content;
 
-    await showModal(suggestionModal, { client, interaction });
+    await showModal(suggestionCreateModal, { client, interaction });
   },
 };
